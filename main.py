2afd023730cf8308fd44fc6019a0fff4de7219d6
PhiColTan <DamLaR1803@gmail.com>
2018-06-15 15:21:10 -0300

    Add quotes importation, tw config creation and tweet send-off at certain time in main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 
 import json
 import sys
+import wikiquote_import as wq_imp
+import twitter_config as tw_conf
+import datetime as dt
 
 # Name: Wikiquote Twitter Bot
 # Desc: A simple way to tweet famous quotes from Wikiquote
@@ -19,5 +22,21 @@ if __name__ == '__main__':
     with open('config.json', 'r') as f:
         config = json.load(f)
 
-    
+    quotes = wq_imp.get_quotes(config)
+
+    quotes_to_send = [pot_tweet for pot_tweet in quotes if len(pot_tweet) <= 280]
+
+    tw_api = tw_conf.create_twitter_config(config)
+
+    used_tweets_idxs = list()
+
+    while len(used_tweets_idxs) < len(quotes_to_send):
+        curr_tm = dt.datetime.now().time()
+
+        if (curr_tm.hour, curr_tm.minute, curr_tm.second, curr_tm.microsecond) == (8, 0, 0, 0):
+            used_tweets_idxs = tw_conf.choose_quote_n_tweet(quotes_to_send, used_tweets_idxs, tw_api)
+
+    print("Add more quotes")
+
+
 
